b1128079ecfc8d1bc1163589d7bf4708007037a4
mbernico <mike.bernico@gmail.com>
2018-03-30 19:51:06 -0500

    created a no transfer learning  model

--- transfer_learning_config.py
+++ transfer_learning_config.py
@@ -2,6 +2,7 @@ class Configuration:
     def __init__(self):
         self.feature_extraction_epochs = 10
         self.fine_tuning_epochs = 20
+        self.epochs_without_transfer_learning = 100
         self.batch_size = 30
         self.data_dir = "data/10-monkey-species/training"
         self.val_dir = "data/10-monkey-species/validation"
---
+++ without_transfer_learning.py
@@ -0,0 +1,97 @@
+# Using 10 Monkey Species Dataset without Transfer Learning
+# https://www.kaggle.com/slothkong/10-monkey-species
+# Author: Mike Bernico @mikebernico mike.bernico@gmail.com
+
+# these seeds are both required for reproducibility
+import numpy as np
+np.random.seed(42)
+import tensorflow as tf
+tf.set_random_seed(42)
+
+from keras.applications.inception_v3 import InceptionV3
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD
+import os
+from transfer_learning_config import Configuration
+
+
+def build_model():
+    # create the base model, using Inception Structure but no weights
+    base_model = InceptionV3(weights=None, include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)  # make sure we're back at a 2d tensor
+    x = Dense(1024, activation='relu')(x)  # add one fully connected layer
+    predictions = Dense(10, activation='softmax')(x)  # 10 monkey, therefore 10 output units w softmax activation
+
+    model = Model(inputs=base_model.input, outputs=predictions)
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def create_callbacks(name):
+    tensorboard_callback = TensorBoard(log_dir=os.path.join(os.getcwd(), "tensorboard_log", name), write_graph=True, write_grads=False)
+    checkpoint_callback = ModelCheckpoint(filepath="./model-weights" + name + ".{epoch:02d}-{val_loss:.6f}.hdf5", monitor='val_loss',
+                                          verbose=0, save_best_only=True)
+    return [tensorboard_callback, checkpoint_callback]
+
+
+def setup_data(train_data_dir, val_data_dir, img_width=299, img_height=299, batch_size=16):
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    val_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+    validation_generator = val_datagen.flow_from_directory(
+        val_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical')
+    return train_generator, validation_generator
+
+
+def fit_model(model, train_generator, val_generator, batch_size, epochs, name):
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=train_generator.n // batch_size,
+        epochs=epochs,
+        validation_data=val_generator,
+        validation_steps=val_generator.n // batch_size,
+        callbacks=create_callbacks(name=name),
+        verbose=1)
+    return model
+
+
+def eval_model(model, val_generator, batch_size):
+    scores = model.evaluate_generator(val_generator, steps=val_generator.n // batch_size)
+    print("Loss: " + str(scores[0]) + " Accuracy: " + str(scores[1]))
+
+
+def main():
+    config = Configuration()
+    train_generator, val_generator = setup_data(config.data_dir, config.val_dir, batch_size=config.batch_size)
+
+    # Feature Extraction
+    model = build_model()
+    model = fit_model(model, train_generator, val_generator,
+                      batch_size=config.batch_size,
+                      epochs=config.epochs_without_transfer_learning,
+                      name="without_transfer_learning")
+
+    print("Training Complete.")
+    eval_model(model, val_generator, batch_size=config.batch_size)
+
+    print("Saving Model...")
+    model.save("no_transfer_learning_model.h5")
+
+
+if __name__ == "__main__":
+    main()
+
+
